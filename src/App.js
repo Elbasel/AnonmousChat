66a4a6ecedb7050ab98251a058a8e57a5ff81753
elbasel <abdelrahman.elbasel@gmail.com>
2022-08-21 01:08:55 +0200

    parse api 20% - untested

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ import { subscribe } from "on-screen-keyboard-detector";
 function App() {
   const [currentView, setCurrentView] = useState("Home");
 
+  // / Import Parse minified version
+  // Your Parse initialization configuration goes here
+  const PARSE_APPLICATION_ID = "2JbJXUS3TCcnQ7WO7wbROMUEc64qCkrpjG8W6Q7w";
+  const PARSE_HOST_URL = "https://parseapi.back4app.com/";
+  const PARSE_JAVASCRIPT_KEY = "UciG0lunmmSEJzIyuhLDn6Jl67uGsp8pDCpkyDhr";
+  Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+  Parse.serverURL = PARSE_HOST_URL;
+
   subscribe((visibility) => {
     if (visibility === "hidden") {
       PubSub.publish("keyboardHidden");
--- src/components/ChatRoomComponents/ChatArea.js
+++ src/components/ChatRoomComponents/ChatArea.js
@@ -1,9 +1,11 @@
 import Message from "./Message";
 import PubSub from "pubsub-js";
 import { useState } from "react";
+import Parse from "parse";
 
 const ChatArea = () => {
   const [raised, setRaised] = useState(false);
+  const [messagesArray, setMessagesArray] = useState([]);
 
   PubSub.subscribe("keyboardHidden", () => {
     setRaised(false);
@@ -15,43 +17,38 @@ const ChatArea = () => {
       document.querySelector(".chatArea").scrollHeight;
   });
 
+  const render = async () => {
+    const query = new Parse.Query("Message").ascending("createdAt");
+    const messages = await query.find();
+    setMessagesArray(messages);
+  };
+
+  render();
+
   return (
     <div
       className={`${
         raised ? "padBottom" : ""
       } chatArea overflow-auto scrollbar-hide flex-1 gap-1 flex flex-col min-h-[607px] mt-[75px] max-h-20 scroll-smooth sm:min-h-[80vh] pt-2`}
     >
-      {[
-        {
-          id: "id-aasaaaaaaaaaaaaaaa aaaaaaaa aaaaaaaaa aaaaaaaaaa aaaaaaaa aaaaaaaa a",
-          username: "elbasel",
-        },
-        { id: "id-b", username: "elbasel" },
-        { id: "id-c", username: "elbasel" },
-        { id: "id-d", username: "elbasel" },
-        { id: "id-e", username: "elbasel" },
-        { id: "id-f", username: "elbasel" },
-        { id: "id-g", username: "pink" },
-        { id: "id-h", username: "elbasel" },
-        { id: "id-i", username: "elbasel" },
-        { id: "id-j", username: "elbasel" },
-        { id: "id-A", username: "pink" },
-      ].map((msg, index, array) => {
-        let prevUser, currentUser, nextUser;
-        let showUsername = false;
-        let profileImgUrl = "https://pbs.twimg.com/media/EZqRRpTUMAABaHI.jpg";
+      {messagesArray.map((msg, index, array) => {
+        console.log(msg);
+        let profileImgUrl = msg.get("profileImgUrl");
 
+        let prevUser, currentUser, nextUser;
+
+        let showUsername = false;
         let groupedMessage = true;
 
         if (array[index - 1]) {
-          prevUser = array[index - 1].username;
+          prevUser = array[index - 1].get("username");
         }
 
         if (array[index + 1]) {
-          nextUser = array[index + 1].username;
+          nextUser = array[index + 1].get("username");
         }
 
-        currentUser = msg.username;
+        currentUser = msg.get("username");
 
         if (currentUser !== nextUser) {
           groupedMessage = false;
@@ -64,13 +61,13 @@ const ChatArea = () => {
         return (
           <Message
             key={msg.id}
-            messageBody={msg.id}
-            username={msg.username}
-            groupedMessage={groupedMessage}
-            sentAt="10:00 pm"
+            messageBody={msg.get("body")}
+            username={msg.get("username")}
+            sentAt={msg.createdAt}
             messageNumber={index}
             profileImgUrl={profileImgUrl}
             showUsername={showUsername}
+            groupedMessage={groupedMessage}
           />
         );
       })}
